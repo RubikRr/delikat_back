42402f6918a36120635dc127877a53c6b286a72a
Ukropchik <vova.chakalov.2003@gmail.com>
2023-03-08 21:09:17 +0300

    убрал автокомлит

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -6,7 +6,7 @@
             @method("patch")
             <div >
                 <div> <label for="name">Назваение</label></div>
-                <div><input type="text" name="name" placeholder="Name" value="{{$product->name}}"></div>
+                <div><input type="text" name="name" placeholder="Name"  autocomplete="off"  value="{{$product->name}}"></div>
             </div>
             <div class="product">
                 <div><label for="img">Картинка</label></div>
@@ -15,7 +15,7 @@
             </div>
             <div class="product">
                 <div><label for="price">Цена</label></div>
-                <div><input type="number" name="price"  placeholder="Number" autocomplete="off" value="{{$product->price}}"></div>
+                <div><input type="number" name="price"  placeholder="Number"  value="{{$product->price}}"></div>
 
             </div>
             <div class="product">
