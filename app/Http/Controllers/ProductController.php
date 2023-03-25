b7d69b2fc729ec94fcaf09453a409e0c835cac0a
Ukropchik <vova.chakalov.2003@gmail.com>
2023-03-26 00:02:56 +0300

    уменьшение фотографии при создании продукта

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,8 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
+use Intervention\Image\ImageManager;
+
 
 class ProductController extends Controller
 {
@@ -29,13 +31,17 @@ class ProductController extends Controller
             'image'=>'Image',
             'price'=>'integer'
         ]);
-
         $filename=$product['image']->getClientOriginalName();
-        //Сохраняем оригинальную картинку
-        $product['image']->move(Storage::path('/public/images/products/').'original',$filename);
+
+        $image= $request->file("image")->getRealPath();
+        $image_resize = Image::make($image);
+        $image_resize->fit(500);
+        $image_resize->save(Storage::path('/public/images/products/').'thumbnail/'.$filename);
+
+        $product['image']->move(Storage::path('/public/images/products/').'original/',$filename);
 
 
-        $product['image']="/images/products/original/".$filename;
+        $product['image']="/images/products/thumbnail/".$filename;
         Product::create($product);
         return redirect()->route("product.showAll");
     }
@@ -84,14 +90,14 @@ class ProductController extends Controller
             [
                 'name'=>'Фэри',
 
-                'image'=>'/images/products/original/corgi_samurai.png',
+                'image'=>'/images/products/thumbnail/corgi_samurai.png',
                 'price'=>76,
                 'description'=>"Крутой средство для мытья посуды"
             ],
             [
                 'name'=>'Порошок',
 
-                'image'=>'/images/products/original/corgi.png',
+                'image'=>'/images/products/thumbnail/corgi.png',
                 'price'=>100,
                 'description'=>"Чистая одежда"
 
@@ -99,14 +105,14 @@ class ProductController extends Controller
             [
                 'name'=>'Подгузники',
 
-                'image'=>'/images/products/original/wolf.png',
+                'image'=>'/images/products/thumbnail/wolf.png',
                 'price'=>200,
                 'description'=>"Для спиногрызов"
             ],
             [
                 'name'=>'Бритва',
 
-                'image'=>'/images/products/original/corgi.png',
+                'image'=>'/images/products/thumbnail/corgi.png',
                 'price'=>1000,
                 'description'=>"Джилет лучще для мужчины нет"
             ]
